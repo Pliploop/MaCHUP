0202323dc35216ff9204d0529235e84726608e6e
Julien Paul Mateo Guinot <jpmg86@frank.eecs.qmul.ac.uk>
2023-10-24 16:39:59 +0100

    Patterns + learned class token

--- src/models/Transformer/Encoder.py
+++ src/models/Transformer/Encoder.py
@@ -2,11 +2,14 @@ from torch import nn
 import torch
 import math
 from pytorch_lightning import LightningModule
+from linear_attention_transformer import LinearAttentionTransformerLM
+from torch.nn import TransformerEncoder, TransformerEncoderLayer, LayerNorm
+from linformer import Linformer
 
 
 
 class Embed(LightningModule):
-    
+
     def __init__(self, embedding_behaviour, embedding_sizes, n_codebooks, card, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.embedding_behaviour = embedding_behaviour
@@ -14,15 +17,18 @@ class Embed(LightningModule):
         self.n_codebooks = n_codebooks
         self.card = card
 
-        self.emb = nn.ModuleList([nn.Embedding(self.card + 1, self.embedding_sizes[codebook]) for codebook in range(self.n_codebooks)])
+        self.emb = nn.ModuleList([nn.Embedding(
+            self.card + 3, self.embedding_sizes[codebook]) for codebook in range(self.n_codebooks)])
 
-    def forward(self,indices):
+        ## +3 for pad, pattern tokens, and mask tokens
+
+    def forward(self, indices):
         B, K, T = indices.shape
 
-        embeddings = [self.emb[k](indices[:, k]) for k in range(K)] ## shape B,T,E
-        print(embeddings[0].shape)
+        embeddings = [self.emb[k](indices[:, k])
+                      for k in range(K)]  # shape B,T,E
         if self.embedding_behaviour == 'sum':
-            
+
             input_ = sum(embeddings)
         else:
             input_ = torch.cat(embeddings, dim=-1)
@@ -37,7 +43,8 @@ class PositionalEncoding(LightningModule):
         self.dropout = nn.Dropout(p=dropout)
 
         position = torch.arange(max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        div_term = torch.exp(torch.arange(0, d_model, 2)
+                             * (-math.log(10000.0) / d_model))
         pe = torch.zeros(max_len, 1, d_model)
         pe[:, 0, 0::2] = torch.sin(position * div_term)
         pe[:, 0, 1::2] = torch.cos(position * div_term)
@@ -54,12 +61,13 @@ class PositionalEncoding(LightningModule):
 
 class LearnedPositionalEncoding(LightningModule):
 
-    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 1024):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 2048):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
 
         # Define a learnable parameter for positional encodings
-        self.positional_encodings = nn.Parameter(torch.randn(max_len, 1, d_model))
+        self.positional_encodings = nn.Parameter(
+            torch.randn(max_len, 1, d_model))
 
     def forward(self, x):
         """
@@ -74,60 +82,101 @@ class LearnedPositionalEncoding(LightningModule):
 
         return self.dropout(x)
 
-class TransformerEncoder(LightningModule):
+
+class Encoder(LightningModule):
+    """"Default transformer encoder. Default behaviour is according to encodecMAE (or similar):
+    
+        sum embeddings of all tokens and conduct masking afterwards (with or without codebook pattern generator).
+    
+    Other possible behaviours :
+    
+        Pattern + Masking before summing embeddings. Meaning the masking mask would include all embeddings. Allows for structured masking patterns like in patchout
+        Pattern + Masking before flattening embeddings. Allows for structured patterns in masking and discarding embeddings *BUT* results in 4x longer sequence
+
+    
+    """
 
     def __init__(
             self,
-            n_codebooks = 4,
-            embedding_size = [512,256,128,64],
-            card = 1024,
-            embedding_behaviour = 'concat',
-            position_encoder = PositionalEncoding,
-            encoder_kind = "linear",
+            n_codebooks=4,
+            embedding_size=[512, 256, 128, 64],
+            card=1024,
+            embedding_behaviour='concat',
+            position_encoder="sinusoidal",
+            sequence_len=2048,
+            layers = 6,
+            n_heads = 8,
             *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self.n_codebooks = n_codebooks
         self.embedding_size = embedding_size
         self.card = card
+        self.sequence_len = sequence_len
         self.embedding_behaviour = embedding_behaviour
         if self.embedding_behaviour == "concat":
             self.d_model = sum(self.embedding_size)
         else:
             self.d_model = self.embedding_size[0]
-        
-        self.position_encoder = position_encoder(self.d_model)
 
-        self.emb = Embed(embedding_behaviour=self.embedding_behaviour,embedding_sizes=self.embedding_size, card=self.card, n_codebooks=self.n_codebooks)
-        self.linears = nn.ModuleList([nn.Linear(self.embedding_size[codebook], self.card) for codebook in range(self.n_codebooks)])
-        
-        self.encoder_kind = encoder_kind
-        
-        if self.encoder_kind == "linear":
-            self.transformer = None
+        if position_encoder == 'sinusoidal':
+            self.position_encoder = PositionalEncoding(
+                self.d_model, max_len=self.sequence_len)
         else:
-            self.transformer = None
+            self.position_encoder = LearnedPositionalEncoding(
+                self.d_model, mex_len=self.sequence_len)
 
-    def forward(self,indices):
-        # x is of shape B,n_q,T
+        self.emb = Embed(embedding_behaviour=self.embedding_behaviour,
+                         embedding_sizes=self.embedding_size, card=self.card, n_codebooks=self.n_codebooks)
+        self.linears = nn.ModuleList([nn.Linear(
+            self.embedding_size[codebook], self.card) for codebook in range(self.n_codebooks)])
+
+        self.n_heads = n_heads
+        self.layers = layers
+
+        self.transformer = None
+        
+        self.norm_in = LayerNorm(self.d_model)
+
+        self.class_token = nn.Parameter(torch.randn(1, 1, self.d_model))
+
+
+    def forward(self, indices):
+        # indices is of shape B,n_q,T
         B, K, T = indices.shape
+        
 
-        ## masking here?
-
-        ## pattern generation here
+        input_ = self.emb(indices)
+        # shape B,T,d_model
+        input_ = self.position_encoder(input_)
+        input_ = self.norm_in(input_)
+        print(input_.shape)
+        class_token = self.class_token.expand(B, 1, self.d_model)  # Expand class token for batch
+        input_ = torch.cat([class_token, input_], dim=1)
+        output_ = self.transformer(input_)
+        # shape B,T,d_model
 
         
-        input_ = self.emb(indices)
+
+        assert output_.shape == (B,T+1,self.d_model), f"{output_.shape}"
+
+        return output_
 
 
 
-        ## norm in here
+class LinearEncoder(Encoder):
+    def __init__(self, n_codebooks=4, embedding_size=[512, 256, 128, 64], card=1024, embedding_behaviour='concat', position_encoder="sinusoidal", sequence_len=2048, layers=6, n_heads=8, *args, **kwargs) -> None:
+        super().__init__(n_codebooks, embedding_size, card, embedding_behaviour, position_encoder, sequence_len, layers, n_heads, *args, **kwargs)
+        self.norm = LayerNorm(self.d_model)
+        self.transformer = Linformer(self.d_model, self.sequence_len, self.layers)
+        
 
-        ## Positional encoding here
+class VanillaEncoder(Encoder):
+    def __init__(self, n_codebooks=4, embedding_size=[512, 256, 128, 64], card=1024, embedding_behaviour='concat', position_encoder="sinusoidal", sequence_len=2048, layers=6, n_heads=8, *args, **kwargs) -> None:
+        super().__init__(n_codebooks, embedding_size, card, embedding_behaviour, position_encoder, sequence_len, layers, n_heads, *args, **kwargs)
+        self.norm = LayerNorm(self.d_model)
+        self.transformer_layer = TransformerEncoderLayer(self.d_model, self.n_heads, activation="gelu")
+        self.transformer = TransformerEncoder(self.transformer_layer, self.layers, norm=self.norm)
 
-        ## Transformer encoder here
-
-        return input_
 
 
-    
--- src/models/model.py
+++ src/models/model.py
@@ -1,15 +1,72 @@
 from torch import nn
 from pytorch_lightning import LightningModule
 from src.models.Encodec import Encodec
-from src.models.Transformer.Encoder import TransformerEncoder
+from src.models.Transformer.Encoder import Encoder
+from audiocraft.modules.codebooks_patterns import DelayedPatternProvider,UnrolledPatternProvider,VALLEPattern
 
 
 class MuMRVQ (LightningModule):
 
-    def __init__(self, encodec: LightningModule = Encodec(), transformer_encoder: LightningModule = TransformerEncoder(), *args, **kwargs) -> None:
+    def __init__(self, encodec: LightningModule, encoder: LightningModule, pattern="delay", n_codebooks = 4, pattern_special_token = 1025, mask_special_token = 1026, mask_before = False, debug = False, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.encodec = encodec
-        self.transformer_encoder = transformer_encoder
+        self.transformer_encoder = encoder
+
+        self.pattern_correspondence = {
+            "delay" : DelayedPatternProvider,
+            "unrolled" : UnrolledPatternProvider,
+            "valle" : VALLEPattern,
+            "none"  : None
+        }
+
+        self.pattern_special_token = pattern_special_token
+        self.mask_special_token = mask_special_token
+        self.pattern_provider = self.pattern_correspondence[pattern](n_q = n_codebooks)
+        self.mask_before = mask_before
+        self.debug = debug
+        
     
     def forward(self,x):
-        return self.transformer_encoder(self.encodec(x))
+        ## x is an array of mono 24kHz audio truncated to a certain length.
+        
+        codes = self.encodec(x)
+        if self.debug : print(f"codes shape : {codes.shape}")
+
+        if self.pattern_provider:
+            codes, indices, pattern_mask = self.get_pattern(codes)             # pattern application
+        
+        if self.debug : print(f"codes after pattern : {codes.shape}")
+        ## pattern and masking here probably
+        
+        if self.mask_before:
+            pass
+
+        encoded = self.transformer_encoder(codes)
+
+        if not self.mask_before:
+            pass
+
+        ## keep encoded for Contrastive loss
+
+        ## reinject masked indices here, not applicable if masked before encoding
+
+        if not self.mask_before:
+            pass ## reinjection of mask tokens
+
+        ## decoder
+
+        ## keep decoded for contrastive loss
+
+        ## classification head
+
+        ## cross-entropy between codes and decoded/encoded
+
+        out_ = encoded
+
+        return out_
+
+    def get_pattern(self,codes):
+        B,K,T = codes.shape
+        pattern = self.pattern_provider.get_pattern(T)
+        new_codes, indices, mask = pattern.build_pattern_sequence(codes,self.pattern_special_token)
+        return new_codes, indices, mask
