4a2225d6558aeaf5141ba97eeb28dec390c6a9c5
Julien Paul Mateo Guinot <jpmg86@frank.eecs.qmul.ac.uk>
2023-11-07 17:45:30 +0000

    logging checkpoint and config in same place

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ from src.dataloading.datasets import CustomAudioDataModule
 from pytorch_lightning.cli import SaveConfigCallback
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer
 from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
 import yaml
+import os
+import shutil
 
 class LoggerSaveConfigCallback(SaveConfigCallback):
     def save_config(self, trainer: Trainer, pl_module: LightningModule, stage: str) -> None:
             if trainer.logger is not None:
-                print('========== wandb logging dir ================')
-                print(trainer.logger.log_dir)
-                print('============ wandb logger save dir =========')
-                print(trainer.logger.save_dir)
-                
                 config = self.parser.dump(self.config, skip_none=False)  # Required for proper reproducibility
                 with open(self.config_filename, "r") as config_file:
                     config = yaml.load(config_file, Loader=yaml.FullLoader)
                     trainer.logger.experiment.config.update(config)
+        
 
 class MyLightningCLI(LightningCLI):
     def add_arguments_to_parser(self, parser):
@@ -29,6 +28,8 @@ class MyLightningCLI(LightningCLI):
         parser.link_arguments("model.encoder.init_args.sequence_len", "model.decoder.init_args.sequence_len")
         parser.link_arguments("data.target_sample_rate","model.encodec.init_args.sample_rate")
         parser.add_argument("--log", default=False)
+        parser.add_argument("--log_model", default=True)
+        parser.add_argument("--ckpt_path", default="MuMRVQ_checkpoints")
 
 if __name__ == "__main__":
     
@@ -39,14 +40,27 @@ if __name__ == "__main__":
 
     
     if cli.config.log:
-        logger = WandbLogger(project="MuMRVQ", log_model=True)
+        logger = WandbLogger(project="MuMRVQ")
     else:
         logger = None
         
+        
+    
     cli.trainer.logger = logger
     
-    cli.trainer.fit(model=cli.model,datamodule=cli.datamodule)
-
-    ## wandb logger here + set callbacks for checkpointing and config saving before running fit with the wandb logger experiment name.
-
+    experiment_name = cli.trainer.logger.experiment.name
+    ckpt_path = cli.config.ckpt_path
+    if not os.path.exists(os.path.join(ckpt_path,experiment_name)):
+        os.makedirs(os.path.join(cli.config.ckpt_path,experiment_name))
+    shutil.copy(cli.config.config[0],os.path.join(cli.config.ckpt_path,experiment_name))
+    if cli.config.log_model:
+        callbacks = [ModelCheckpoint(os.path.join(cli.config.ckpt_path,experiment_name),monitor="val_crossentropy_simple",save_top_k=1)]
+        cli.trainer.callbacks = cli.trainer.callbacks + callbacks
+        
     
+        
+        
+    
+        
+    
+    cli.trainer.fit(model=cli.model,datamodule=cli.datamodule)
